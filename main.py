6a3d4dd3bda1a0603d825ad07c4d4e7e5f118271
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-07-25 15:47:26 -0700

    created a calculator with the ai

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+if len(sys.argv) == 1:
+    raise Exception("No prompt given")
 
-print(response.text)
+user_prompt = sys.argv[1]
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    print(f"User prompt: {user_prompt}")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+else:
+    print(response.text)
