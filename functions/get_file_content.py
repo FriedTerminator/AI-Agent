cf1653df690cf3bf84ad72da8f525ec54da22ce4
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-07-28 14:30:44 -0700

    able to read contents of files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -10,10 +10,10 @@ def get_file_content(working_directory, file_path):
         if not full_path.startswith(working_directory_abs):
             return f"Error: Cannot read \"{file_path}\" as it is outside the permitted working directory"
         
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(full_path):
             return f"Error: File is not found or is not a regular file: \"{file_path}\""
         
-        with open(file_path, "r") as f:
+        with open(full_path, "r") as f:
             file_content_string = f.read(MAX_CHARACTERS)
             return file_content_string
     except Exception as e:
