3dc3ae0d1b7ef99261880f8d02aaa95a1d10e61c
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-08-04 10:36:55 -0700

    created function to write to files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -14,7 +14,10 @@ def get_file_content(working_directory, file_path):
             return f"Error: File is not found or is not a regular file: \"{file_path}\""
         
         with open(full_path, "r") as f:
-            file_content_string = f.read(MAX_CHARACTERS)
+            file_content_string = f.read()
+            if len(file_content_string) > MAX_CHARACTERS:
+                file_content_string = file_content_string[:MAX_CHARACTERS]
+                file_content_string += f"\n[...File \"{full_path}\" truncated at 10000 characters]"
             return file_content_string
     except Exception as e:
         return f"Error: {str(e)}"
